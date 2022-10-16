b69b8226e7308be61716094c64370cd0a3273d5b
Syed Umair Ali <57952190+SyedUmairCodes@users.noreply.github.com>
2022-10-16 23:05:21 +0500

    changed logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { Stack } from "@mui/material"
 import { Link } from "react-router-dom"
-import { logo } from "../utils/constants.js"
+import  logo  from "../assets/logo.png"
 import {SearchBar} from "./index.js"
 
 const Navbar = () => (
